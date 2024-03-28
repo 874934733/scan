f3278b4671b1f5a8a9eae3bb9c898680872a958e
874934733@qq.com <wang@96121>
2024-03-28 15:52:11 +0800

    Initial commit

---
+++ app/src/main/java/com/yingyang/scan/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yingyang.scan
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yingyangfly.scan.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
